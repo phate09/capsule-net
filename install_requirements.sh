51e2df8a3dcfdba5ff5d0f0be4646b752fcd6bf9
phate09 <edoardo.bacci.developer@gmail.com>
2018-12-17 14:39:38 +0000

    updated requirements

---
+++ install_requirements.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+conda install pytorch torchvision -c pytorch
+pip install visdom
+pip install git+https://github.com/pytorch/tnt.git@master
+pip install jupyter
+pip install tqdm
