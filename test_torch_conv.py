e5e5f90d5c89c0837443021bf2d00705b44dfef4
edoardo <phate09@hotmail.it>
2019-07-21 14:13:23 +0100

    added test for im2col algorithm

--- test_torch_conv.py
+++ test_torch_conv.py
@@ -1,12 +1,13 @@
 import numpy as np
 import torch.nn
 import torch.nn
+import torch.nn.functional as F
 
 from plnn.simplified.conv_net2 import Net
 from plnn.simplified.conv_net_convert import convert_conv2d, get_weights
 
 
-def test1(): # DEPRECATED
+def test1():  # DEPRECATED
     """This test ensures that the conversion between conv2d and linear works, returned value should be 0.0
     """
     batch_size = 1
@@ -120,8 +121,64 @@ def test3():
     print(f'Test3 is {"NOT " if np.abs(l2 - l).max() > 0 else ""}PASSED')
 
 
+def test4():
+    """test conversion of stride2 conv layer NOT WORKING"""
+    batch_size = 128
+    torch.set_default_dtype(torch.float64)  # forces to use double precision as default
+    y = torch.tensor(np.random.rand(batch_size, 1, 20, 20))
+    y = F.pad(y, (0, 1, 0, 1), 'constant', 0)
+    x = torch.nn.Conv2d(1, 20, (9, 9), (2, 2))
+    l = x(y).detach().numpy()
+    input_size = y.shape[1:]
+    # W = x.weight
+    # b = x.bias
+    weights, param, size = convert_conv2d(x.weight, x.bias, input_size, stride=(2, 2))
+    W, b = weights[0], weights[1]
+    lin1 = torch.nn.Linear(28 * 28, b.size)
+    lin1.weight.data = torch.tensor(W, dtype=torch.double)
+    lin1.bias.data = torch.tensor(b, dtype=torch.double)
+    l2 = lin1(y.view(batch_size, -1)).detach()
+    l2 = l2.reshape(l.shape).detach().numpy()
+    print(l.shape)
+    print(l2.shape)
+    print(np.abs(l2 - l).max())  # number should be negligible
+    print(f'Test3 is {"NOT " if np.abs(l2 - l).max() > 1e-13 else ""}PASSED')
+
+
+def test5():
+    """got the test from https://pytorch.org/docs/stable/nn.html#torch.nn.Unfold"""
+    torch.set_default_dtype(torch.float64)
+    inp = torch.randn(1, 3, 10, 12)
+    w = torch.randn(2, 3, 4, 5)
+    inp_unf = torch.nn.functional.unfold(inp, (4, 5))  # im2col operation
+    out_unf = inp_unf.transpose(1, 2).matmul(w.view(w.size(0), -1).t()).transpose(1, 2)
+    # out = torch.nn.functional.fold(out_unf, (7, 8), (1, 1))
+    # or equivalently (and avoiding a copy),
+    out = out_unf.view(1, 2, 7, 8)
+    print((torch.nn.functional.conv2d(inp, w) - out).abs().max())
+
+
+def test6():
+    """testing the im2col (unfold in pytorch) algorithm for batch multiplication of convolutional layers"""
+    torch.set_default_dtype(torch.float64)
+    batch_size = 128
+    inp = torch.randn(batch_size, 1, 28, 28)
+    w = torch.randn(20, 1, 9, 9)
+    inp_unf = torch.nn.functional.unfold(inp, (9, 9), stride=2)  # im2col operation
+    out_unf = inp_unf.transpose(1, 2).matmul(w.view(w.size(0), -1).t()).transpose(1, 2)
+    # out = torch.nn.functional.fold(out_unf, (7, 8), (1, 1))
+    # or equivalently (and avoiding a copy),
+    out = out_unf.view(128, 20, 10, 10)
+    error = (torch.nn.functional.conv2d(inp, w, stride=2) - out).abs().max()
+    print(error)
+    print(f'Test3 is {"NOT " if error > 1e-13 else ""}PASSED')
+
+
 if __name__ == '__main__':
     # test1() DEPRECATED
-    test1_5()
+    # test1_5()
     # test2()
     # test3()
+    # test4() #not working, replacing with im2col
+    # test5()
+    test6()
